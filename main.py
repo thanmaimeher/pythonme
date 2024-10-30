0f449786a24697b019b54bfa536a9785ff6b023b
thanmaimeher <103480409+thanmaimeher@users.noreply.github.com>
2024-10-30 21:21:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a simple Python program
+
+# Print "Hello, World!"
+print("Hello, World!")
+
+# Sum two numbers and print the result
+num1 = 5
+num2 = 7
+sum = num1 + num2
+print("Sum of", num1, "and", num2, "is", sum)
